49e3bd9fa779d9819a4bffd642ade6b78502cefa
xiaolan <16525372+xiaolanqqai@users.noreply.github.com>
2019-06-23 21:39:11 +0800

    Update search_ajx.js

--- js/search_ajx.js
+++ js/search_ajx.js
@@ -2,21 +2,31 @@ var oTxt = document.getElementById("txt");
 var oList = document.getElementById("list");
 var oBtn = document.getElementById("btn");
 var oBox = document.getElementById("box");
+
+var times
 //键盘按键松开时发生：
 oTxt.onkeyup = function(){
 	oList.style.display = "block";
 	var oValue = oTxt.value;
 	var oScript = document.createElement("script");
-	oScript.src = "https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd="+oValue+"&cb=fly";
+	oScript.src = "https://sp0.baidu.com/su?wd="+oValue+"&cb=fly";
 	document.body.appendChild(oScript);
 	document.body.removeChild(oScript);
 }
-//oTxt失去焦点时发生：
-oTxt.onblur = function(){
-	oList.style.display = "none";
+
+//当从搜索框或者列表移开时，如果600毫秒内移回列表或者搜索框则关闭定时器，否则隐藏列表
+oTxt.onmouseleave = oList.onmouseleave = function(){
+	oList.onmouseover = oTxt.onmouseover = function() {
+		clearTimeout(times);
+	}
+	times=setTimeout(function() {
+		oList.style.display = "none";
+	},600)
+	
 }
-//oTxt获取焦点时发生：
-oTxt.onfocus = function(){
+
+//当单击搜索框是发生，显示列表
+oTxt.onmousedown = function(){
 	oList.style.display = "block";
 }
 //通过for循环遍历获得的数据
